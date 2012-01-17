c6f4dadef48ec377c1e38a0b354d6bc85b1fcee9
Micha? Sapalski <sapalskimichal@gmail.com>
2012-01-17 15:00:31 +0100

    Initial commit - empty android project.

---
+++ src/pl/edu/uj/tcs/kuini/KuiniActivity.java
@@ -0,0 +1,13 @@
+package pl.edu.uj.tcs.kuini;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KuiniActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
